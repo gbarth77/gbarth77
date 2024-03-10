efd7fe7a3a44e8933976914475fa82967c85e455
Gustavo Barth <157074385+gbarth77@users.noreply.github.com>
2024-03-10 00:01:38 -0300

    Social interaction
    Putting into work my "creating classes and methods" lesson introduction to creat a basic social friend interacting system, here you will find simple methods, such as for add friends, removing friends and sending confirmed messages to your friends.

---
+++ Friendship/Program.cs
@@ -0,0 +1,63 @@
+﻿class Program
+{
+    /*Basic friend request, removing and interacting system,
+    for interacting. Use the HelloFriend method to send a
+    confirmed message to a friend, for adding a friend use
+    AddFriend method, for removing a friend that you may do
+    not like anymore or just want to remove, use RemoveFriend
+    method.*/
+    public class Messages
+    {
+        static void Main(string[] args)
+        {
+            Messages.HelloFriend();
+        }
+        public static string? friendMessage;
+
+        public static void AddFriend()
+        {
+            // adding a friend  
+            int friends;
+            System.Console.WriteLine("1 friend added! Congrats!");
+            friends = 1;
+            System.Console.WriteLine($"You have now {friends} friends!");
+        }
+        public static void RemoveFriend()
+        {
+            // friend removal
+            int friends;
+            System.Console.WriteLine("Friend removed.");
+            friends = -1;
+            System.Console.WriteLine($"Now you have {friends} friends");
+        }
+        public static void HelloFriend()
+        {
+            // This is for sending a confirmed message for some of your friends
+            string? confirmedMessage = "Hello! My dear friend!";
+            do
+            {
+                System.Console.WriteLine("Send a message for a friend!");
+                string? friendMessage = Console.ReadLine();
+                if (friendMessage != "")
+                {
+                    System.Console.WriteLine($"Are you sure that you want to send this message?\n '{friendMessage}'");
+                    System.Console.WriteLine("Type 'y' if you want to send it or 'n' if you want to delete it");
+                    string? confirm = Console.ReadLine();
+                    if (confirm == "y")
+                    {
+                        confirmedMessage = friendMessage;
+                        System.Console.WriteLine($"Your message was sent! Check in here: {confirmedMessage}");
+                    }
+                    else if (confirm == "n")
+                    {
+                        friendMessage = null;
+                    }
+                }
+                else
+                {
+                    System.Console.WriteLine("Please type a valid message...");
+                }
+            } while (friendMessage == null);
+        }
+    }
+}
