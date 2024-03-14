5efc119bf0e5e0e40ca185101a7b2602ef9fa383
Gustavo Barth <gbarth7777@gmail.com>
2024-03-14 19:05:08 -0300

    obj modified

--- obj/Debug/net8.0/Friendship.AssemblyInfo.cs
+++ obj/Debug/net8.0/Friendship.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Friendship")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d262f382351b472002f322ddfa719486d60387f1")]
 [assembly: System.Reflection.AssemblyProductAttribute("Friendship")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Friendship")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
